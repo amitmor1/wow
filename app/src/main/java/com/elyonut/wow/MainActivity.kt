92c570b1fe46792a6c028699580d90b0dc1ab7cd
amitmor <amit.mor.org@gmail.com>
2019-06-12 12:25:23 +0300

    added permission request and show first location

--- app/src/main/java/com/elyonut/wow/MainActivity.kt
+++ app/src/main/java/com/elyonut/wow/MainActivity.kt
@@ -1,17 +1,41 @@
 package com.elyonut.wow
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.location.LocationEngineRequest
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.CompassEngine
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), PermissionsListener, OnMapReadyCallback {
+
+    private val defaultIntervalMilliseconds = 1000L
+    private val defaultMaxWaitTime = defaultIntervalMilliseconds * 5
+
     private lateinit var mapView: MapView
     private lateinit var map: MapboxMap
+
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationEngine: LocationEngine
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +49,57 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(mapboxMap: MapboxMap) {
         map = mapboxMap
 
-        mapboxMap.setStyle(Style.MAPBOX_STREETS)
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            startLocationService(style)
+        }
+    }
+
+    private fun startLocationService(loadedMapStyle: Style) {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        enableLocationComponent(loadedMapStyle)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            val myLocationComponentOptions = LocationComponentOptions.builder(this)
+                .trackingGesturesManagement(true)
+                .accuracyColor(ContextCompat.getColor(this, R.color.myLocationColor)).build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                .locationComponentOptions(myLocationComponentOptions).build()
+
+            map.locationComponent.apply {
+
+                activateLocationComponent(locationComponentActivationOptions)
+
+                isLocationComponentEnabled = true
+
+                cameraMode = CameraMode.TRACKING
+
+                renderMode = RenderMode.COMPASS
+            }
+
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            if (map.style != null) {
+                enableLocationComponent(map.style!!)
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.permission_not_granted), Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 
     override fun onResume() {
