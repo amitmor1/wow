1b61dac190b7d842fa63f8333153cc1175b433f8
georgeomalley <georgeomalley1@yandex.com>
2019-08-28 17:56:54 +0300

    editing filter fragment

--- app/src/main/java/com/elyonut/wow/FilterFragment.kt
+++ app/src/main/java/com/elyonut/wow/FilterFragment.kt
@@ -25,7 +25,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [FilterFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FilterFragment : Fragment(),  {
+class FilterFragment : Fragment(), AdapterView.OnItemSelectedListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
