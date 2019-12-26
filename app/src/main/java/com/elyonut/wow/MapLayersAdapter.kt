08817fda93b197206f385f54772947d9fd530031
Ferero <fererorocher@yandex.com>
2019-12-26 09:39:38 +0200

    changed map_item ImageButton to ImageView

--- app/src/main/java/com/elyonut/wow/MapLayersAdapter.kt
+++ app/src/main/java/com/elyonut/wow/MapLayersAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -30,7 +31,7 @@ class MapLayersAdapter(var context: Context, var mapLayers: ArrayList<String>) :
     }
 
     class MapLayersViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val mapTypeImage: ImageButton = view.findViewById(R.id.map_type_image)
+        val mapTypeImage: ImageView = view.findViewById(R.id.map_type_image)
         val mapDescription: TextView = view.findViewById(R.id.map_type_description)
     }
 
